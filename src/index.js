b74751e6045a6a702a51ea13ffa0b976272604e4
Objectivitix <79152594+Objectivitix@users.noreply.github.com>
2023-05-09 20:31:53 -0400

    Delete stylesInit.js

--- src/index.js
+++ src/index.js
@@ -1,4 +1,7 @@
-import "./stylesInit";
+import "./styles/global.css";
+import "./styles/header.css";
+import "./styles/sidebar.css";
+import "./styles/main.css";
 
 import { loadProjectsList, loadThisWeek } from "./app";
 
--- src/stylesInit.js
+++
@@ -1,4 +0,0 @@
-import "./styles/global.css";
-import "./styles/header.css";
-import "./styles/sidebar.css";
-import "./styles/main.css";
